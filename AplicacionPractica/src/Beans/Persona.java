39935251529ee5121956d0ebb2e10e60a643e3e8
CESDE <CESDE@PC-21>
2018-10-10 08:36:45 -0500

    Agregando nuevo paquete

---
+++ AplicacionPractica/src/Beans/Persona.java
@@ -0,0 +1,69 @@
+package Beans;
+
+/**
+ *
+ * @author CESDE
+ */
+public class Persona {
+    
+    private int id_persona;
+    private String nombre;
+    private String apePaterno;
+    private String apeMaterno;
+
+    /**
+     * @return the id_persona
+     */
+    public int getId_persona() {
+        return id_persona;
+    }
+
+    /**
+     * @param id_persona the id_persona to set
+     */
+    public void setId_persona(int id_persona) {
+        this.id_persona = id_persona;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apePaterno
+     */
+    public String getApePaterno() {
+        return apePaterno;
+    }
+
+    /**
+     * @param apePaterno the apePaterno to set
+     */
+    public void setApePaterno(String apePaterno) {
+        this.apePaterno = apePaterno;
+    }
+
+    /**
+     * @return the apeMaterno
+     */
+    public String getApeMaterno() {
+        return apeMaterno;
+    }
+
+    /**
+     * @param apeMaterno the apeMaterno to set
+     */
+    public void setApeMaterno(String apeMaterno) {
+        this.apeMaterno = apeMaterno;
+    }
+}
