a126400e1080cf29403779f34207438aa7a2af75
CESDE <CESDE@PC-21>
2018-10-10 08:18:25 -0500

    subiendo el proyecto inicial

---
+++ AplicacionPractica/src/aplicacionpractica/AplicacionPractica.java
@@ -0,0 +1,12 @@
+package aplicacionpractica;
+
+public class AplicacionPractica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
